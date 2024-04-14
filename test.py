d1aad604a33a08717dbff37301b3d743a6674fa9
Prakashmani <awasthiprakashmani7@gmail.com>
2024-04-14 16:16:58 +0530

    exception class completed and tested

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 from src.Heart_Attack_Risk_Analyzer_Project.logger import logging
+from src.Heart_Attack_Risk_Analyzer_Project.exception import HeartRiskException
+import os, sys
+logging.info("testing exception")
 
-logging.info("this is a test log")
+try:
+    raise Exception("This is a test Exception")
+except Exception as e:
+    raise HeartRiskException(e,sys) from e
